e6618d11cc677989482f54b687202a6a2e7ef812
Someone you must not mess up with..... <khandelwalshivansh840@gmail.com>
2025-11-12 19:35:27 +0530

    Create Leap Year Checker.py

---
+++ Leap Year Checker.py
@@ -0,0 +1,10 @@
+# Logical operators
+print("Leap Year Checker")
+print("=================")
+
+year = int(input("Enter a year: "))
+
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(f"{year} is a Leap Year")
+else:
+    print(f"{year} is not a Leap Year")
